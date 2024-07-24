eaf21894cdf6f312953817fb240854997cfc99c7
Damien Vincelot <damien.vincelot@gmail.com>
2024-07-24 16:01:12 +0200

    Update

--- main.py
+++ main.py
@@ -3,22 +3,3 @@ from controllers.main_controller import MainController
 if __name__ == "__main__":
     run = MainController()
     run.main_menu()
-
-"""
-Important :
-    - Optimiser maincontroller
-
-Bugs :
-Chargement tournoi, meme si scores ont déjà été saisi
-    Il faut les saisir à nouveau pour passer au round suivant
-
-Améliorations:
-    - Vérifier le format des inputs pour chaque saisie
-    - Vérifier qu'un numéro de round n'existe pas déjà pour éviter
-        la duplication des rounds
-
-
-Accessoire :
-    - is_first plutot que name *
-    - Indiquer les méthodes publiques et privées
-"""
